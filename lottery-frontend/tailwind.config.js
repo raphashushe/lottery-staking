5e6cddb042daf13d2116e1f53ce8dea9ddf78853
Rapha <raphacubrich@gmail.com>
2024-11-26 12:03:27 +0100

    Add Homepage with Sidebar Navigation and Interactive Design for Lottery DApp

---
+++ lottery-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        bounce: "bounce 1s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
